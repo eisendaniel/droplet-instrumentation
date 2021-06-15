e78b17549c90002f9aebfe96ac56a054e0a095ce
eisendaniel <danieleisen99@gmail.com>
2021-06-15 18:47:35 +1200

    begin serial input parsing

--- Software/Motor_Control/src/main.cpp
+++ Software/Motor_Control/src/main.cpp
@@ -1,6 +1,8 @@
 
 #include <ESP_FlexyStepper.h>
 
+#define INPUT_SIZE 30
+
 // IO pin assignments
 const int R_MOTOR_STEP_PIN = 19;
 const int R_MOTOR_DIRECTION_PIN = 18;
@@ -22,23 +24,53 @@ void setup()
   r_stage.connectToPins(R_MOTOR_STEP_PIN, R_MOTOR_DIRECTION_PIN);
   z_stage.connectToPins(Z_MOTOR_STEP_PIN, Z_MOTOR_DIRECTION_PIN);
 
-  r_stage.setSpeedInStepsPerSecond(10 * REV_STEPS);
+  r_stage.setSpeedInStepsPerSecond(1 * REV_STEPS);
   z_stage.setSpeedInStepsPerSecond(10 * REV_STEPS);
 
-  r_stage.setAccelerationInStepsPerSecondPerSecond(100 * REV_STEPS);
-  r_stage.setDecelerationInStepsPerSecondPerSecond(100 * REV_STEPS);
-s
+  r_stage.setAccelerationInStepsPerSecondPerSecond(10 * REV_STEPS);
+  r_stage.setDecelerationInStepsPerSecondPerSecond(10 * REV_STEPS);
+
   z_stage.setAccelerationInStepsPerSecondPerSecond(100 * REV_STEPS);
   z_stage.setDecelerationInStepsPerSecondPerSecond(100 * REV_STEPS);
+
+  Serial.println("Adjust Start point...");
+
+  // Calculate based on max input size expected for one command
+
+  // Get next command from Serial (add 1 for final 0)
+  char input[INPUT_SIZE + 1];
+  byte size = Serial.readBytes(input, INPUT_SIZE);
+  // Add the final 0 to end the C string
+  input[size] = 0;
+
+  // Read each command pair
+  char *command = strtok(input, "&");
+  while (command != 0)  
+  {
+    // Split the command in two values
+    char *separator = strchr(command, ':');
+    if (separator != 0)
+    {
+      // Actually split the string in 2: replace ':' with 0
+      *separator = 0;
+      int servoId = atoi(command);
+      ++separator;
+      int position = atoi(separator);
+
+      // Do something with servoId and position
+    }
+    // Find the next command in input string
+    command = strtok(0, "&");
+  }
 }
 
 void loop()
 {
   r_stage.moveRelativeInSteps(REV_STEPS / 4);
-  z_stage.moveRelativeInSteps(REV_STEPS);
+  r_stage.moveRelativeInSteps(0);
   delay(1000);
 
   r_stage.moveRelativeInSteps(-(REV_STEPS / 4));
-  z_stage.moveRelativeInSteps(REV_STEPS);
+  r_stage.moveRelativeInSteps(0);
   delay(1000);
 }
