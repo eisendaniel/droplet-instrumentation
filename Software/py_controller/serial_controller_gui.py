d2b20f744fcb1b883f4b7274c7b814ff51ccabea
eisendaniel <danieleisen99@gmail.com>
2021-11-26 13:35:11 +1300

    grid layout cleaneed

--- Software/py_controller/serial_controller_gui.py
+++ Software/py_controller/serial_controller_gui.py
@@ -2,8 +2,20 @@ import sys
 from PyQt5 import QtCore, QtWidgets, QtSerialPort
 from datetime import datetime
 
-BAUDS = ["110", "300", "1200", '2400', '4800', '9600',
-         '19200', '38400', '57600', '115200', '128000', '256000']
+BAUDS = [
+    "110",
+    "300",
+    "1200",
+    "2400",
+    "4800",
+    "9600",
+    "19200",
+    "38400",
+    "57600",
+    "115200",
+    "128000",
+    "256000",
+]
 
 
 def get_com_devices():
@@ -14,7 +26,7 @@ def get_com_devices():
 class App(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title = 'Droplet Instrumentation Controller'
+        self.title = "Droplet Instrumentation Controller"
         self.left = 64
         self.top = 128
         self.width = 600
@@ -56,10 +68,7 @@ class Widget(QtWidgets.QWidget):
         self.raw_input = QtWidgets.QLineEdit()
         self.raw_input.setPlaceholderText("Raw Input...")
         self.raw_input.returnPressed.connect(self.send_raw)
-        self.send_btn = QtWidgets.QPushButton(
-            text="Send",
-            clicked=self.send_raw
-        )
+        self.send_btn = QtWidgets.QPushButton(text="Send", clicked=self.send_raw)
         self.do_ts = QtWidgets.QCheckBox(text="Show Timestamp")
         self.serial_monitor = QtWidgets.QTextEdit(readOnly=True)
         self.serial_monitor.setFontFamily("Monospace")
@@ -76,61 +85,72 @@ class Widget(QtWidgets.QWidget):
         self.Z_val.setValue(5.0)
         self.DEL_val = QtWidgets.QDoubleSpinBox()
         self.R_btn = QtWidgets.QPushButton(
-            text="Rotate (deg)", clicked=(lambda: self.send_cmd("R "+str(self.R_val.value()))))
+            text="Rotate (deg)",
+            clicked=(lambda: self.send_cmd("R " + str(self.R_val.value()))),
+        )
         self.Z_btn = QtWidgets.QPushButton(
-            text="Set Height (mm)", clicked=(lambda: self.send_cmd("Z "+str(self.Z_val.value()))))
+            text="Set Height (mm)",
+            clicked=(lambda: self.send_cmd("Z " + str(self.Z_val.value()))),
+        )
         self.DEL_btn = QtWidgets.QPushButton(
-            text="Delay (ms)", clicked=(lambda: self.send_cmd("DEL "+str(self.DEL_val.value()))))
+            text="Delay (ms)",
+            clicked=(lambda: self.send_cmd("DEL " + str(self.DEL_val.value()))),
+        )
         self.PIP_btn = QtWidgets.QPushButton(
-            text="Trigger Pipette", clicked=self.send_P)
-
-        self.record_seq = QtWidgets.QPushButton(
-            text="Record Sequence",
-            checkable=True,
-            toggled=self.record_toggled
+            text="Trigger Pipette", clicked=self.send_P
         )
 
-        self.res_btn = QtWidgets.QPushButton(text = f"Go to Reservoir ({self.res_pos}°)", clicked=(lambda: self.send_cmd(f"R {self.res_pos}")))
-        self.drop_btn = QtWidgets.QPushButton(text = f"Go to Droplet Stage ({self.drop_pos}°)", clicked=(lambda: self.send_cmd(f"R {self.drop_pos}")))
-        self.reset_btn = QtWidgets.QPushButton(text = "Reset Saved Positions", clicked=self.init_reset)
+        self.record_seq = QtWidgets.QPushButton(
+            text="Record Sequence", checkable=True, toggled=self.record_toggled
+        )
+
+        self.res_btn = QtWidgets.QPushButton(
+            text=f"Go to Reservoir ({self.res_pos}°)",
+            clicked=(lambda: self.send_cmd(f"R {self.res_pos}")),
+        )
+        self.drop_btn = QtWidgets.QPushButton(
+            text=f"Go to Droplet Stage ({self.drop_pos}°)",
+            clicked=(lambda: self.send_cmd(f"R {self.drop_pos}")),
+        )
+        self.reset_btn = QtWidgets.QPushButton(
+            text="Reset Saved Positions", clicked=self.init_reset
+        )
 
         self.com_select = QtWidgets.QComboBox()
         self.com_select.setDuplicatesEnabled(False)
         self.com_select.addItems(self.devices)
-        self.com_select.setCurrentIndex(self.com_select.count()-1)
+        self.com_select.setCurrentIndex(self.com_select.count() - 1)
         self.baud_select = QtWidgets.QComboBox()
         self.baud_select.addItems(BAUDS)
         self.baud_select.setCurrentIndex(BAUDS.index("115200"))
 
         self.connection_btn = QtWidgets.QPushButton(
-            text="Connect",
-            checkable=True,
-            toggled=self.on_toggled
+            text="Connect", checkable=True, toggled=self.on_toggled
         )
 
         self.Motors.layout = QtWidgets.QGridLayout()  # row, col, r_span, c_span
-        self.Motors.layout.addWidget(self.raw_input,      0, 0, 1, 2)
-        self.Motors.layout.addWidget(self.send_btn,       0, 2, 1, 1)
-        self.Motors.layout.addWidget(self.do_ts,          0, 3, 1, 1)
+        self.Motors.layout.addWidget(self.raw_input, 0, 0, 1, 2)
+        self.Motors.layout.addWidget(self.send_btn, 0, 2, 1, 1)
+        self.Motors.layout.addWidget(self.do_ts, 0, 3, 1, 1)
         self.Motors.layout.addWidget(self.serial_monitor, 1, 0, 1, 4)
 
-        self.Motors.layout.addWidget(self.reset_btn,      3, 0, 1, 1)
-        self.Motors.layout.addWidget(self.res_btn,        4, 0, 1, 1)
-        self.Motors.layout.addWidget(self.drop_btn,       5, 0, 1, 1)
+        self.Motors.layout.addWidget(self.reset_btn, 2, 1, 1, 2)
+        self.Motors.layout.addWidget(self.res_btn, 3, 1, 1, 1)
+        self.Motors.layout.addWidget(self.drop_btn, 3, 2, 1, 1)
 
-        self.Motors.layout.addWidget(self.home_btn,       2, 1, 1, 2)
-        self.Motors.layout.addWidget(self.R_val,          3, 1, 1, 1)
-        self.Motors.layout.addWidget(self.R_btn,          3, 2, 1, 1)
-        self.Motors.layout.addWidget(self.Z_val,          4, 1, 1, 1)
-        self.Motors.layout.addWidget(self.Z_btn,          4, 2, 1, 1)
-        self.Motors.layout.addWidget(self.DEL_val,        5, 1, 1, 1)
-        self.Motors.layout.addWidget(self.DEL_btn,        5, 2, 1, 1)
-        self.Motors.layout.addWidget(self.record_seq,     6, 1, 1, 1)
-        self.Motors.layout.addWidget(self.PIP_btn,        6, 2, 1, 1)
+        self.Motors.layout.addWidget(self.home_btn, 4, 1, 1, 2)
+        self.Motors.layout.addWidget(self.R_val, 5, 1, 1, 1)
+        self.Motors.layout.addWidget(self.R_btn, 5, 2, 1, 1)
+        self.Motors.layout.addWidget(self.Z_val, 6, 1, 1, 1)
+        self.Motors.layout.addWidget(self.Z_btn, 6, 2, 1, 1)
+        self.Motors.layout.addWidget(self.DEL_val, 7, 1, 1, 1)
+        self.Motors.layout.addWidget(self.DEL_btn, 7, 2, 1, 1)
+        self.Motors.layout.addWidget(self.record_seq, 8, 1, 1, 1)
+        self.Motors.layout.addWidget(self.PIP_btn, 8, 2, 1, 1)
 
-        self.Motors.layout.addWidget(self.connection_btn, 7, 0, 1, 4)
-        self.Motors.layout.addWidget(self.com_select,     8, 0, 1, 2)
-        self.Motors.layout.addWidget(self.baud_select,    8, 2, 1, 2)
+        self.Motors.layout.addWidget(self.connection_btn, 9, 0, 1, 4)
+        self.Motors.layout.addWidget(self.com_select, 10, 0, 1, 2)
+        self.Motors.layout.addWidget(self.baud_select, 10, 2, 1, 2)
         self.Motors.setLayout(self.Motors.layout)
 
         self.layout.addWidget(self.tabs)
@@ -140,8 +160,9 @@ class Widget(QtWidgets.QWidget):
         self.serial = QtSerialPort.QSerialPort(
             self.com_select.currentText(),
             baudRate=QtSerialPort.QSerialPort.BaudRate(
-                int(self.baud_select.currentText())),
-            readyRead=self.receive
+                int(self.baud_select.currentText())
+            ),
+            readyRead=self.receive,
         )
 
     @QtCore.pyqtSlot()
@@ -183,7 +204,7 @@ class Widget(QtWidgets.QWidget):
     def receive(self):
         while self.serial.canReadLine():
             text = self.serial.readLine().data().decode()
-            text = text.rstrip('\r\n')
+            text = text.rstrip("\r\n")
             if self.do_ts.isChecked():
                 text = str(datetime.now()) + ": " + text
             self.serial_monitor.append(text)
@@ -201,20 +222,20 @@ class Widget(QtWidgets.QWidget):
     @QtCore.pyqtSlot()
     def send_cmd(self, cmd):
         if self.record_seq.isChecked():
-            self.raw_input.setText(self.raw_input.text()+cmd+"; ")
+            self.raw_input.setText(self.raw_input.text() + cmd + "; ")
         else:
             self.serial.write(cmd.encode())
 
-    @ QtCore.pyqtSlot()
+    @QtCore.pyqtSlot()
     def send_P(self):
-        cmd = "PIP "+("UP" if self.pip_engaged else "DOWN")
+        cmd = "PIP " + ("UP" if self.pip_engaged else "DOWN")
         if self.record_seq.isChecked():
-            self.raw_input.setText(self.raw_input.text()+cmd+"; ")
+            self.raw_input.setText(self.raw_input.text() + cmd + "; ")
         else:
             self.serial.write(cmd.encode())
         self.pip_engaged = not self.pip_engaged
 
-    @ QtCore.pyqtSlot(bool)
+    @QtCore.pyqtSlot(bool)
     def record_toggled(self, checked):
         self.raw_input.clear()
         if checked:
@@ -234,7 +255,7 @@ class Widget(QtWidgets.QWidget):
         self.com_select.setCurrentIndex(i)
         self.update_timer.start(2000)
 
-    @ QtCore.pyqtSlot(bool)
+    @QtCore.pyqtSlot(bool)
     def on_toggled(self, checked):
         self.connection_btn.setText("Disconnect" if checked else "Connect")
         if checked:
@@ -242,8 +263,9 @@ class Widget(QtWidgets.QWidget):
                 self.serial = QtSerialPort.QSerialPort(
                     self.com_select.currentText(),
                     baudRate=QtSerialPort.QSerialPort.BaudRate(
-                        int(self.baud_select.currentText())),
-                    readyRead=self.receive
+                        int(self.baud_select.currentText())
+                    ),
+                    readyRead=self.receive,
                 )
                 if not self.serial.open(QtCore.QIODevice.ReadWrite):
                     self.connection_btn.setChecked(False)
@@ -257,5 +279,5 @@ def main():
     sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
